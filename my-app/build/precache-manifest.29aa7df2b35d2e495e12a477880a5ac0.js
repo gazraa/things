d245b501a15668d2a69abdee133d74e624174ae4
Goose <gaz@gazraa.com>
2019-01-02 01:01:56 +0000

    production build

--- my-app/build/precache-manifest.29aa7df2b35d2e495e12a477880a5ac0.js
+++ my-app/build/precache-manifest.29aa7df2b35d2e495e12a477880a5ac0.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "fda7dc4834c6f15dfb5e",
-    "url": "/things/static/css/main.0b8a7396.chunk.css"
+    "revision": "2e51b208b50feb329115",
+    "url": "/things/static/css/main.d27849a1.chunk.css"
   },
   {
-    "revision": "fda7dc4834c6f15dfb5e",
-    "url": "/things/static/js/main.fda7dc48.chunk.js"
+    "revision": "2e51b208b50feb329115",
+    "url": "/things/static/js/main.2e51b208.chunk.js"
   },
   {
     "revision": "8377f37c1cf84a8ae598",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/things/static/js/runtime~main.e49818d9.js"
   },
   {
-    "revision": "7b753647b1c60ce53e32022c15172fd0",
+    "revision": "a9d5c74661642b75f46eed026d17f7d6",
     "url": "/things/index.html"
   }
 ];
