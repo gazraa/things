120cdc67466f82f681c64d45361ffd4a15414441
Goose <gaz@gazraa.com>
2019-01-02 00:16:25 +0000

    Updates

---
+++ precache-manifest.172cf037c609247848656f8f11ab0aeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fda7dc4834c6f15dfb5e",
+    "url": "/things/static/css/main.0b8a7396.chunk.css"
+  },
+  {
+    "revision": "fda7dc4834c6f15dfb5e",
+    "url": "/things/static/js/main.fda7dc48.chunk.js"
+  },
+  {
+    "revision": "8377f37c1cf84a8ae598",
+    "url": "/things/static/js/1.8377f37c.chunk.js"
+  },
+  {
+    "revision": "e49818d92614430c36bd",
+    "url": "/things/static/js/runtime~main.e49818d9.js"
+  },
+  {
+    "revision": "7b753647b1c60ce53e32022c15172fd0",
+    "url": "/things/index.html"
+  }
+];
